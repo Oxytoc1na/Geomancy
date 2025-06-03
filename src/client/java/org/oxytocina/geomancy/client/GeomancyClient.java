6dc20ea714fe7f7675129245699d02758982b480
Mannatu <mannatu@mannatu.com>
2025-06-03 14:13:11 +0200

    Initial commit

---
+++ src/client/java/org/oxytocina/geomancy/client/GeomancyClient.java
@@ -0,0 +1,10 @@
+package org.oxytocina.geomancy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GeomancyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/oxytocina/geomancy/Geomancy.java
@@ -0,0 +1,10 @@
+package org.oxytocina.geomancy;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Geomancy implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
