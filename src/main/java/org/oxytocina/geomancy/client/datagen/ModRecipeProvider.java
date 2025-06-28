a3b4f44279a80f0252377dcf7c21553626ba8401
Tina-Joos <165283853+Tina-Joos@users.noreply.github.com>
2025-06-28 16:52:05 +0200

    smithing recipe refactor

--- src/main/java/org/oxytocina/geomancy/client/datagen/ModRecipeProvider.java
+++ src/main/java/org/oxytocina/geomancy/client/datagen/ModRecipeProvider.java
@@ -5,17 +5,11 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.advancement.criterion.CriterionConditions;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.server.recipe.*;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.predicate.item.ItemPredicate;
-import net.minecraft.recipe.Ingredient;
-import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.Registries;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
 import org.oxytocina.geomancy.Geomancy;
@@ -25,12 +19,11 @@ import org.oxytocina.geomancy.client.datagen.recipes.SmitheryRecipeJsonBuilder;
 import org.oxytocina.geomancy.items.ModItems;
 import org.oxytocina.geomancy.items.jewelry.JewelryItem;
 import org.oxytocina.geomancy.recipe.*;
-import org.oxytocina.geomancy.registries.ModRecipeTypes;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
@@ -79,33 +72,46 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
         // smithing recipes
         {
+            // Empty Artifact
             AddSmitheryRecipe(Arrays.stream(new SmithingIngredient[] {
                             SmithingIngredient.ofItems(ModItems.MITHRIL_INGOT),
                             SmithingIngredient.ofItems(Items.LEATHER)
                     }).toList(),ModItems.EMPTY_ARTIFACT,1, 40,5, true,
                     conditionsFromItem(ModItems.MITHRIL_INGOT));
 
-            AddSmitheryRecipe(Arrays.stream(new SmithingIngredient[] {
-                            SmithingIngredient.ofItems(1,1,1,Items.IRON_INGOT),
-                            SmithingIngredient.ofItems(1,1,3,Items.IRON_INGOT),
-                            SmithingIngredient.ofItems(1,1,5,Items.IRON_INGOT),
-                            SmithingIngredient.ofItems(1,1,7,Items.IRON_INGOT),
-                    }).toList(),ModItems.IRON_RING,1, 30,15, false,
-                    conditionsFromItem(Items.IRON_INGOT));
-            AddSmitheryRecipe(Arrays.stream(new SmithingIngredient[] {
-                            SmithingIngredient.ofItems(1,1,0,Items.IRON_INGOT),
-                            SmithingIngredient.ofItems(1,1,2,Items.IRON_INGOT),
-                            SmithingIngredient.ofItems(1,1,3,Items.IRON_INGOT),
-                            SmithingIngredient.ofItems(1,1,5,Items.IRON_INGOT),
-                            SmithingIngredient.ofItems(1,1,7,Items.IRON_INGOT),
-                    }).toList(),ModItems.IRON_NECKLACE,1, 50,15, false,
-                    conditionsFromItem(Items.IRON_INGOT));
+            // Iron Ring
+            AddShapedSmitheryRecipe(new String[]{
+                            " o ",
+                            "o o",
+                            " o "}
+                    ,new SPatKey[]{new SPatKey("o",SmithingIngredient.ofItems(1,1,1,Items.IRON_INGOT))},
+                    ModItems.IRON_RING,1,30,15,conditionsFromItem(Items.IRON_INGOT));
+
+            // Iron Necklace
+            AddShapedSmitheryRecipe(new String[]{
+                            "o o",
+                            "o o",
+                            " o "}
+                    ,new SPatKey[]{new SPatKey("o",SmithingIngredient.ofItems(1,1,1,Items.IRON_INGOT))},
+                    ModItems.IRON_NECKLACE,1,30,15,conditionsFromItem(Items.IRON_INGOT));
+
+            // mithril hammer
+            AddShapedSmitheryRecipe(new String[]{
+                            "ooo",
+                            "oso",
+                            " s "}
+                    ,new SPatKey[]{
+                            new SPatKey("o",SmithingIngredient.ofItems(1,1,1,ModItems.MITHRIL_INGOT)),
+                            new SPatKey("s",SmithingIngredient.ofItems(1,1,1,Items.STICK)),
+                    },
+                    ModItems.MITHRIL_HAMMER,1,100,12,conditionsFromItem(ModItems.MITHRIL_INGOT));
 
             // jewelry recipes
             for(JewelryItem item : JewelryItem.List){
                 AddSmitheryJewelryRecipe(item);
             }
 
+            // Artifact of Iron
             AddSmitheryRecipe(Arrays.stream(new SmithingIngredient[] {
                             SmithingIngredient.ofItems(1,1,4,ModItems.EMPTY_ARTIFACT),
                             SmithingIngredient.ofItems(1,1,1,Items.IRON_BLOCK),
@@ -113,6 +119,16 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                             SmithingIngredient.ofItems(1,1,5,Items.IRON_CHESTPLATE),
                             SmithingIngredient.ofItems(1,1,7,Items.IRON_BARS),
                     }).toList(),ModItems.ARTIFACT_OF_IRON,1, 100,20,false,
+                    conditionsFromItem(ModItems.EMPTY_ARTIFACT));
+
+            // Artifact of Gold
+            AddSmitheryRecipe(Arrays.stream(new SmithingIngredient[] {
+                            SmithingIngredient.ofItems(1,1,4,ModItems.EMPTY_ARTIFACT),
+                            SmithingIngredient.ofItems(1,1,1,Items.GOLD_BLOCK),
+                            SmithingIngredient.ofItems(1,1,3,Items.GOLDEN_APPLE),
+                            SmithingIngredient.ofItems(1,1,5,Items.GOLDEN_HELMET),
+                            SmithingIngredient.ofItems(1,1,7,Items.GOLDEN_CARROT),
+                    }).toList(),ModItems.ARTIFACT_OF_GOLD,1, 100,20,false,
                     conditionsFromItem(ModItems.EMPTY_ARTIFACT));
         }
 
@@ -138,6 +154,41 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     private void AddSmitheryRecipe(List<SmithingIngredient> input, ItemConvertible output, int outputCount, int requiredProgress, int difficulty, boolean shapeless, CriterionConditions conditions){
         AddSmitheryRecipe(input,output,outputCount,requiredProgress,difficulty, shapeless,"default_conditions",conditions);
+    }
+
+    private void AddShapedSmitheryRecipe(String[] map, SPatKey[] entries,
+                                         ItemConvertible output, int outputCount, int requiredProgress, int difficulty,
+                                         CriterionConditions conditions)
+    {
+        List<SmithingIngredient> ingredients = new ArrayList<>();
+        for (int i = 0; i < map.length; i++) {
+            String partial = map[i];
+            for (int j = 0; j < partial.length(); j++) {
+                String c = partial.substring(j,j+1);
+                for(SPatKey entry : entries){
+                    if(Objects.equals(entry.key, c)){
+                        ingredients.add(entry.toIngredient(i*partial.length()+j));
+                        break;
+                    }
+                }
+            }
+        }
+        AddSmitheryRecipe(ingredients,output,outputCount,requiredProgress,difficulty,false,conditions);
+    }
+
+    private static class SPatKey {
+        public String key;
+        public SmithingIngredient ingredient;
+
+        public SPatKey(String key, SmithingIngredient ingredient){
+            this.key=key;
+            this.ingredient=ingredient;
+        }
+
+        public SmithingIngredient toIngredient(int slot){
+            SmithingIngredient res = new SmithingIngredient(ingredient.ingredient,ingredient.count,slot,ingredient.mishapWeight);
+            return res;
+        }
     }
 
     private void AddSmitheryRecipe(List<SmithingIngredient> input, ItemConvertible output, int outputCount, int requiredProgress, int difficulty, boolean shapeless, String criterionName, CriterionConditions conditions){
