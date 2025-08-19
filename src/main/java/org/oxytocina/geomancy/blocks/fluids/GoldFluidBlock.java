cb9c7d2ecb28509c6d4589e988ff4213bd340d77
Tina-Joos <165283853+Tina-Joos@users.noreply.github.com>
2025-08-19 15:26:07 +0200

    molten gold fluid collision

--- src/main/java/org/oxytocina/geomancy/blocks/fluids/GoldFluidBlock.java
+++ src/main/java/org/oxytocina/geomancy/blocks/fluids/GoldFluidBlock.java
@@ -1,12 +1,15 @@
 package org.oxytocina.geomancy.blocks.fluids;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -15,6 +18,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.oxytocina.geomancy.blocks.ModBlocks;
+import org.oxytocina.geomancy.registries.ModFluidTags;
 
 public class GoldFluidBlock extends ModFluidBlock {
 
@@ -40,6 +45,24 @@ public class GoldFluidBlock extends ModFluidBlock {
 
     @Override
     public @Nullable BlockState handleFluidCollision(World world, @NotNull FluidState state, @NotNull FluidState otherState) {
-        return null;
+        // unobstructed
+        if(otherState.isEmpty() || otherState.isIn(ModFluidTags.MOLTEN_GOLD))
+            return null;
+
+        if(otherState.isIn(FluidTags.WATER))
+        {
+            // extinguish
+            return Blocks.GOLD_BLOCK.getDefaultState();
+        }
+
+        if(otherState.isIn(FluidTags.LAVA)){
+            // ???
+            if(otherState.isOf(Fluids.LAVA))
+                return Blocks.RAW_GOLD_BLOCK.getDefaultState();
+            else
+                return ModBlocks.GILDED_DEEPSLATE.getDefaultState();
+        }
+
+        return Blocks.GOLD_ORE.getDefaultState();
     }
 }
